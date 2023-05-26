c8b4cf3fb69b2dfd950ada580cc3e67ba14a57ff
yagob2 <127253988+yagob2@users.noreply.github.com>
2023-05-26 17:03:44 -0300

    Atualizacao dos controllers e endpointcollection

--- src/main/java/com/residencia/ecommerce/controllers/ClienteController.java
+++ src/main/java/com/residencia/ecommerce/controllers/ClienteController.java
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.residencia.ecommerce.entities.Cliente;
 import com.residencia.ecommerce.services.ClienteService;
 
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	@Autowired
