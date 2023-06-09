9adf3f9c995b94c7e691973eb9bd201f73435906
Paulo Vitor Lessa <paulo.vitor.lessa@gmail.com>
2023-06-09 13:18:33 -0300

    a

--- src/main/java/com/residencia/ecommerce/controllers/ItemPedidoController.java
+++ src/main/java/com/residencia/ecommerce/controllers/ItemPedidoController.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import com.residencia.ecommerce.dto.ItemPedidoDTO;
 import com.residencia.ecommerce.services.ItemPedidoService;
 
 import jakarta.validation.Valid;
-
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping("/itempedidos")
 public class ItemPedidoController {
--- src/main/java/com/residencia/ecommerce/controllers/ProdutoController.java
+++ src/main/java/com/residencia/ecommerce/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import com.residencia.ecommerce.dto.ProdutoDTO;
 import com.residencia.ecommerce.services.ProdutoService;
 
 import io.jsonwebtoken.io.IOException;
-
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
